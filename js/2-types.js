5f317d55c2774a29d425b6b157adea0d3538cc89
Parker K Catalano <parkerkcatalano@Admins-MacBook-Pro.local>
2018-10-22 19:40:32 -0400

    First Commit

---
+++ js/2-types.js
@@ -0,0 +1,22 @@
+/******************
+ * 01 - Declaration
+ ******************/
+let username: string = 'instructor'
+let instructorid: number = 123456789
+const password: string = 'letmein123'
+
+/*****************
+ * 02 - Basic Types
+ *******************/
+let arr: string[];
+arr = ['yes', 'maybe', 'no']
+
+let numbers: Array<number> = [1,2,3,4,5]
+
+let quints: [number, string] = [1, 'Parker']
+
+let whatever: any = '12345'
+/****************
+ * 03 - Union Types
+ ****************/
+//# sourceMappingURL=2-types.js.map
